ea6a3ea3e0d3ce714edc93782394da97c2d2a0a0
Noel Barrera <70553615+noelbarrera@users.noreply.github.com>
2023-12-05 20:45:45 -0500

    add link stylign and remove svg

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,41 +7,23 @@ const Navbar = () => {
   return (
     <nav className="flex flex-row justify-between text-white mt-11">
       <ul className="flex flex-row">
-        <li className="mr-2">
+        <li className="mr-3 hover:underline">
           <Link href="/">About</Link>
         </li>
-        <li>
+        <li className="hover:underline">
           <Link href="/">Projects</Link>
         </li>
       </ul>
 
       <ul className="flex flex-row">
-        <li className="mr-2">
-          <Link className=" flex flex-row" href="/">
+        <li className="mr-3 hover:underline">
+          <Link className=" flex flex-row" href="https://github.com/noelbarrera">
             Github
-            <svg
-              className="fill-current text-white self-center"
-              xmlns="http://www.w3.org/2000/svg"
-              height="15"
-              viewBox="0 -960 960 960"
-              width="24"
-            >
-              <path d="m216-160-56-56 464-464H360v-80h400v400h-80v-264L216-160Z" />
-            </svg>
           </Link>
         </li>
-        <li>
-          <Link className="flex flex-row" href="www.linkedin.com/in/noel-barrera" passHref={true}>
+        <li className="hover:underline">
+          <Link className="flex flex-row" href="https://www.linkedin.com/in/noel-barrera/">
             Linkedin
-            <svg
-              className="fill-current text-white self-center"
-              xmlns="http://www.w3.org/2000/svg"
-              height="15"
-              viewBox="0 -960 960 960"
-              width="24"
-            >
-              <path d="m216-160-56-56 464-464H360v-80h400v400h-80v-264L216-160Z" />
-            </svg>
           </Link>
         </li>
       </ul>
